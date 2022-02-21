da75bfa443378d488d6a2af3f00d94fc4707adc4
Biruk-ak <birukaklilu0110@gmail.com>
2022-02-21 15:34:46 +0300

    Ship Pilots mobile screen and offline sync service helpers

---
+++ mobile/src/screens/pilots/PilotsMobileScreen.tsx
@@ -0,0 +1,79 @@
+/**
+ * Aviation Operations Platform
+ * Module: Pilots
+ * Layer: mobile/screen
+ * Enterprise aviation management suite.
+ * Copyright (c) Biruk-ak — All rights reserved.
+ */
+
+
+// React Native screen for field / crew operations
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Pressable, StyleSheet } from 'react-native';
+
+export function PilotsMobileScreen() {
+  const [rows, setRows] = useState<any[]>([]);
+  useEffect(() => {
+    fetch('/api/v1/pilots').then((r) => r.json()).then((j) => setRows(j.data?.items || []));
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pilots</Text>
+      <FlatList
+        data={rows}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Pressable style={styles.row}>
+            <Text>{item.code} — {item.name}</Text>
+            <Text>{item.status}</Text>
+          </Pressable>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  styleBlock1: { padding: 1, margin: 1 },
+  styleBlock2: { padding: 2, margin: 2 },
+  styleBlock3: { padding: 3, margin: 3 },
+  styleBlock4: { padding: 4, margin: 4 },
+  styleBlock5: { padding: 5, margin: 5 },
+  styleBlock6: { padding: 6, margin: 6 },
+  styleBlock7: { padding: 7, margin: 7 },
+  styleBlock8: { padding: 8, margin: 0 },
+  styleBlock9: { padding: 9, margin: 1 },
+  styleBlock10: { padding: 10, margin: 2 },
+  styleBlock11: { padding: 11, margin: 3 },
+  styleBlock12: { padding: 0, margin: 4 },
+  styleBlock13: { padding: 1, margin: 5 },
+  styleBlock14: { padding: 2, margin: 6 },
+  styleBlock15: { padding: 3, margin: 7 },
+  styleBlock16: { padding: 4, margin: 0 },
+  styleBlock17: { padding: 5, margin: 1 },
+  styleBlock18: { padding: 6, margin: 2 },
+  styleBlock19: { padding: 7, margin: 3 },
+  styleBlock20: { padding: 8, margin: 4 },
+  styleBlock21: { padding: 9, margin: 5 },
+  styleBlock22: { padding: 10, margin: 6 },
+  styleBlock23: { padding: 11, margin: 7 },
+  styleBlock24: { padding: 0, margin: 0 },
+  styleBlock25: { padding: 1, margin: 1 },
+  styleBlock26: { padding: 2, margin: 2 },
+  styleBlock27: { padding: 3, margin: 3 },
+  styleBlock28: { padding: 4, margin: 4 },
+  styleBlock29: { padding: 5, margin: 5 },
+  styleBlock30: { padding: 6, margin: 6 },
+  styleBlock31: { padding: 7, margin: 7 },
+  styleBlock32: { padding: 8, margin: 0 },
+  styleBlock33: { padding: 9, margin: 1 },
+  styleBlock34: { padding: 10, margin: 2 },
+  styleBlock35: { padding: 11, margin: 3 },
+  styleBlock36: { padding: 0, margin: 4 },
+  styleBlock37: { padding: 1, margin: 5 },
+  styleBlock38: { padding: 2, margin: 6 },
+  styleBlock39: { padding: 3, margin: 7 },
+  container: { flex: 1, padding: 16 },
+  title: { fontSize: 22, marginBottom: 12 },
+  row: { paddingVertical: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' },
+});
